5428ca25b40179a8b251bc602b6cffb36291adb6
hehaowen00 <edmundhe@outlook.com>
2021-10-10 20:59:25 +1000

    restructure project and make backend project

---
+++ frontend/src/components/Dashboard.tsx
@@ -0,0 +1,12 @@
+import React, { Fragment, ReactElement, useState } from "react";
+
+import "./styles/Dashboard.css"
+
+function Dashboard(): ReactElement {
+  return (
+    <Fragment>
+    </Fragment>
+  );
+}
+
+export default Dashboard;
