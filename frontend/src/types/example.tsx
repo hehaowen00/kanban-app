b2ba9560671dc2dea38f719371ede9cd585393b7
hehaowen00 <edmundhe@outlook.com>
2022-02-03 23:23:54 +1000

    Move cards out of list objects

--- frontend/src/types/example.tsx
+++ frontend/src/types/example.tsx
@@ -1,23 +1,32 @@
 import { v4 as uuidV4 } from "uuid";
-const ExampleBoard = {
-  id: uuidV4(),
-  name: "Kanban",
-  lists: [
-    {
-      id: uuidV4(),
-      name: "List 1",
-      cards: [
-        {
-          id: uuidV4(),
-          title: "Hello, World!",
-          description: "",
-          attachments: [] as any[],
-          comments: [] as any[],
-          checklists: [] as any[],
-        }
-      ]
-    }
-  ],
-};
+
+function newExampleBoard()  {
+  let cardId = uuidV4();
+
+  let cards: any = {};
+  cards[cardId] = {
+    id: uuidV4(),
+    title: "Hello, World!",
+    description: "",
+    attachments: [] as any[],
+    comments: [] as any[],
+    checklists: [] as any[],
+  };
+
+  return {
+    id: uuidV4(),
+    name: "Kanban",
+    cards,
+    lists: [
+      {
+        id: uuidV4(),
+        name: "List 1",
+        cardIds: [cardId]
+      }
+    ],
+  };
+}
+
+const ExampleBoard = newExampleBoard();
 
 export default ExampleBoard;
